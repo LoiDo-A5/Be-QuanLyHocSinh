e366cc14511f85efd8ddb016af6cd0ef1d474bea
doloiEoh <do.loi@eoh.io>
2025-03-18 13:53:33 +0700

    fix code

--- accounts/api/add_student_to_class.py
+++ accounts/api/add_student_to_class.py
@@ -22,11 +22,28 @@ class AddStudentToClass(APIView):
         class_id = request.data.get('class_id')
 
         if not student_id or not class_id:
-            return Response({'error': 'Both student_id and class_id are required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Cả student_id và class_id đều là bắt buộc'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
+            existing_class = ClassStudent.objects.filter(student_id=student_id).first()
+            if existing_class:
+                return Response({'error': 'Học sinh này đã được phân vào một lớp khác'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            class_obj = ClassName.objects.get(id=class_id)
+            if class_obj.number_of_students >= 40:
+                return Response({'error': 'Lớp này đã đủ số học sinh'}, status=status.HTTP_400_BAD_REQUEST)
+
             class_student = ClassStudent.objects.create(student_id=student_id, class_name_id=class_id)
+
+            class_obj.number_of_students += 1
+            class_obj.save()
+
             return Response({'success': True, 'data': ClassStudentSerializer(class_student).data},
                             status=status.HTTP_201_CREATED)
+        except ClassName.DoesNotExist:
+            return Response({'error': 'Mã lớp không hợp lệ'}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'error': 'Mã học sinh không hợp lệ'}, status=status.HTTP_400_BAD_REQUEST)
         except ValidationError as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
