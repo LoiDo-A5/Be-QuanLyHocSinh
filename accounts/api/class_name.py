fe660cebea2333d60a900b29af42c5de63f640e7
doloiEoh <do.loi@eoh.io>
2025-04-05 13:44:18 +0700

    fix code

--- accounts/api/class_name.py
+++ accounts/api/class_name.py
@@ -4,15 +4,25 @@ from accounts.models.class_name import ClassName
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class ClassNameSerializer(serializers.ModelSerializer):
-    level_name = serializers.SerializerMethodField()
+    level_name = serializers.CharField(source='level.level_name', read_only=True)
 
     class Meta:
         model = ClassName
         fields = ['id', 'level', 'class_name', 'number_of_students', 'level_name']
 
-    def get_level_name(self, obj):
-        return obj.level.level_name if obj.level else None
+    def update(self, instance, validated_data):
+        instance.class_name = validated_data.get('class_name', instance.class_name)
+        instance.number_of_students = validated_data.get('number_of_students', instance.number_of_students)
+
+        level_data = validated_data.get('level', None)
+        if level_data:
+            instance.level = level_data
+
+        instance.save()
+        return instance
+
 
 class ClassNameViewSet(viewsets.ModelViewSet):
     queryset = ClassName.objects.all()
@@ -29,3 +39,4 @@ class ClassNameViewSet(viewsets.ModelViewSet):
             return super().update(request, *args, **kwargs)
         except PermissionError as e:
             return Response({'error': str(e)}, status=status.HTTP_403_FORBIDDEN)
+
